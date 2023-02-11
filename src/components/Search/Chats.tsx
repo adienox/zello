1e58a65011fc0f3bc2efb06dd9dae90c10ddbc09
Adienox <adwaitadk@tuta.io>
2023-02-11 14:26:48 +0545

    optimized the code and added comments

--- src/components/Search/Chats.tsx
+++ src/components/Search/Chats.tsx
@@ -13,35 +13,34 @@ const Chats = () => {
   const { chat, dispatch } = useContext(ChatContext) as ChatContext;
   const [chats, setChats] = useState<DocumentData>();
 
+  // Set up a real-time subscription to the user's chats data
   useEffect(() => {
-    const getChats = () => {
-      const cleanup = onSnapshot(doc(db, "userChats", user.uid), (doc) => {
-        setChats(doc.data());
-      });
-      return () => {
-        cleanup();
-      };
+    if (!user.uid) return; // Exit if the user isn't authenticated
+
+    const unsubscribe = onSnapshot(doc(db, "userChats", user.uid), (doc) => {
+      setChats(doc.data()); // Update the chat data in state
+    });
+    return () => {
+      unsubscribe(); // Clean up the subscription when unmounting
     };
-    user.uid && getChats();
   }, [user]);
 
+  // Transform the chat data into an array of ChatItem components
   const chatItems = chats
     ? Object.entries(chats)
-        .sort((a, b) => b[1].date - a[1].date)
+        .sort((a, b) => b[1].date - a[1].date) // Sort the entries by date
         .map((doc) => {
           if (chat.chatId === doc[0]) {
-            return <ChatItems key={doc[0]} details={doc} active={true} />;
+            return <ChatItems key={doc[0]} details={doc} active />;
           }
           return <ChatItems key={doc[0]} details={doc} />;
         })
     : [];
 
-  const activeItems = chatItems.filter((chat) => {
-    if (chat.props.active) {
-      return chat;
-    }
-  });
+  // Filter the chat items to find the active one
+  const activeItems = chatItems.filter((chat) => chat.props.active);
 
+  // Reset the chat context if there is no active chat item
   useEffect(() => {
     if (activeItems.length === 0) {
       dispatch({ type: "RESET" });
@@ -50,12 +49,12 @@ const Chats = () => {
 
   return (
     <div className="h-[calc(100vh-2.5rem-60px)] overflow-scroll pt-3 pb-[90px] scrollbar-hide md:h-[calc(100%-54px)] md:pb-3">
-      {chatItems.length !== 0 ? (
+      {chatItems.length ? (
         chatItems
       ) : (
         <div className="mx-auto mt-10 flex flex-col items-center rounded bg-blueHighlight p-10 text-white drop-shadow-2xl md:w-[40%]">
           <h3 className="text-3xl">Get started!</h3>
-          <p className="text-sm text-center">
+          <p className="text-center text-sm">
             Search users from the search bar and get to know each other!
           </p>
           <Image src={Reading} className="w-[80%]" alt="" />
